49903976ebeb819a87f1be60452ffbc4ad65d8ed
WIJESINGHE W.P.S.D. <74785686+Pasindu-sd@users.noreply.github.com>
2025-10-23 23:46:47 +0530

    Update Log_Analyzer.py

--- Log_Analyzer.py
+++ Log_Analyzer.py
@@ -2,5 +2,5 @@ import re
 
 with open("/var/log/auth/log") as log:
    for line in log:
-      if re.search(r"Failed password", line):
+      if re.search(r"Failed password", line) or re.search(r"Accepted password", line):
          print(line.strip())
